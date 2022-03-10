c35ef364052fd80ac2f0975b7750910b903b9a0a
dthirst <demian.keihsler@imp.ac.at>
2022-03-10 16:21:09 +0100

    initial commit

---
+++ write_title_to_pdf_metadata.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env conda run -n pdfrw python3
+"""Written by Demian Keihsler at the Institute of Molecular Pathology, Vienna, Austria, 2022-03-10"""
+import os, sys, argparse
+from pdfrw import PdfReader, PdfWriter
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-p', '--path', type=str)
+args = parser.parse_args()
+
+for root, dirs, files in os.walk(os.path.normpath(args.path)):
+    path = root.split(os.sep)
+    for file in  files:
+        if '.pdf' in file:
+            try:
+                pdffile = PdfReader(os.path.join(root, file))
+                try:
+                    pdffile.Info.Title = file.split(' - ')[2]
+                    print(file.split(' - ')[2])
+                except:
+                    pdffile.Info.Title = file
+                    print(f'fail: {file}')
+                PdfWriter(os.path.join(root, file), trailer = pdffile).write()
+            except Exception as e:
+                print(e)
+                pass
